5140916812caf4167c44b97429dbbd525cac0093
Arkadiusz Buras <macbury@gmail.com>
2016-05-27 11:02:12 +0200

    [#120222581] Make consumer enumerable

--- app/consumers/play_station/store_consumer.rb
+++ app/consumers/play_station/store_consumer.rb
@@ -2,24 +2,32 @@
 module PlayStation
   # This class uses PlayStation store json api to retrive list of games
   class StoreConsumer
+    include Enumerable
     URL = 'https://store.playstation.com'.freeze
-    attr_reader :page
+    attr_reader :page, :games
 
     def initialize
       @api = Faraday.new(url: URL) do |faraday|
         faraday.adapter Faraday.default_adapter
         faraday.response :json
       end
-      @page = 0
+      @page  = 0
+      @games = []
     end
 
     # Fetch next page with games
-    # @return list of games
+    # @return true if fetched any games
     def next
       @page += 1
       response = @api.get("/chihiro-api/viewfinder/PL/en/19/STORE-MSF75508-PS4CAT?game_content_type=games&platform=ps4&size=30&gkb=#{@page}&geoCountry=PL")
-      response.body
-      []
+      @games   = response.body['links']
+      return !@games.blank?
+    end
+
+    def each(&block)
+      while self.next
+        @games.each { |game| block.call(game) }
+      end
     end
   end
 end
--- spec/consumers/play_station/store_consumer_spec.rb
+++ spec/consumers/play_station/store_consumer_spec.rb
@@ -1,12 +1,18 @@
 require 'rails_helper'
 
-describe PlayStation::StoreConsumer, vcr: 'games' do
+describe PlayStation::StoreConsumer do
   let(:consumer) { @consumer ||= PlayStation::StoreConsumer.new }
 
-  it 'should fetch first 30 games' do
-    games = consumer.next
+  it 'should fetch first 30 games', vcr: 'first-30-games' do
+    expect(consumer.next).to be(true)
+    games = consumer.games
     expect(games).not_to be_empty
     expect(games.size).to eq(30)
     expect(consumer.page).to eq(1)
+  end
+
+  it 'should fetch next 30 games', vcr: '60-games' do
+    2.times { consumer.next }
+    expect(consumer.page).to eq(2)
   end
 end
