8376b96252339b373fdb44f762482d15471c980c
Arkadiusz Buras <macbury@gmail.com>
2016-05-31 11:37:50 +0200

    [#120533433] Base tests for game model

--- spec/models/game_spec.rb
+++ spec/models/game_spec.rb
@@ -1,5 +1,17 @@
 require 'rails_helper'
 
 describe Game, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it { is_expected.to validate_presence_of(:name) }
+  it { is_expected.to validate_uniqueness_of(:name) }
+  it { is_expected.to validate_presence_of(:description) }
+  it { is_expected.to validate_presence_of(:ps_id) }
+  it { is_expected.to validate_uniqueness_of(:ps_id) }
+
+  it { is_expected.to have_many(:screenshoots).dependent(:destroy) }
+  it { is_expected.to have_many(:prices).dependent(:destroy) }
+
+  it 'generates permalink after create' do
+    game = create(:game, name: 'Battlefield 4')
+    expect(game.permalink).to eq('battlefield-4')
+  end
 end
